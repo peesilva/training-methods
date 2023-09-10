ce4e0a7fc130af5ac6537700df1d837ba1b20b09
Pedro Silva <arbeit.pedro.silva@gmail.com>
2023-09-10 04:13:29 -0300

    exercise add value in method 'add'

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+class Calculator {
+    public int add(int x, int y) {
+        int sum = new Calculator().add(1, 2);
+
+        System.out.println(sum);
+        return sum;
+    }
+}
+
+// int sum = new Calculator().add(1, 2); entendido que aqui contem os valores do metodo add na linha 3
